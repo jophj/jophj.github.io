e2befc35ae977de9753080e981ee016e63263e92
Giovanni Pinto <jop.mezzana@gmail.com>
2015-06-15 23:02:13 +0200

    Updated reddit-steam-tools

--- reddit-steam-tools/scripts/app.js
+++ reddit-steam-tools/scripts/app.js
@@ -8,7 +8,7 @@
 	app.factory('SteamAppProvider',['$http', function ($http) {
 			
 		var HOST = 'localhost:3666';
-		HOST = 'reddit-steam-tools-server.herokuapp.com'
+		HOST = 'reddit-steam-tools-server.herokuapp.com';
 		var iSteamAppsApi = '/ISteamApps/GetAppList/v2/';
 		var appDB = [
 					{
@@ -74,26 +74,43 @@
 					
 		return{
 			initDB: function (callback) {
-				var iSteamAppsRequest = new XMLHttpRequest();
-				
-				iSteamAppsRequest.onload = function (evt) {
-					appDB = JSON.parse(iSteamAppsRequest.responseText).applist.apps;
-					
-					if(callback)
+				$http.get('http://' + HOST + iSteamAppsApi).then(
+					function(resp){
+						appDB = resp.data.applist.apps;
 						callback();
-				};
+					}
+				);
+
+				// var iSteamAppsRequest = new XMLHttpRequest();
 				
-				iSteamAppsRequest.open("GET", 'http://' + HOST + iSteamAppsApi);
-				iSteamAppsRequest.send();
+				// iSteamAppsRequest.onload = function (evt) {
+				// 	appDB = JSON.parse(iSteamAppsRequest.responseText).applist.apps;
+					
+				// 	if(callback)
+				// 		callback();
+				// };
+				
+				// iSteamAppsRequest.open("GET", 'http://' + HOST + iSteamAppsApi);
+				// iSteamAppsRequest.send();
 			},
 			
 			search: function (searchString) { //TODO split words and add .+
-				var regexp = new RegExp(searchString, 'i');
+				var tokens = searchString.split(' ');
 				var toReturn = [];
+
 				appDB.forEach(function(app) {
-					if(app.name.search(regexp) >= 0)
+					var match = true;
+
+					tokens.forEach(function(token){
+						var regexp = new RegExp(token, 'i');
+						if(app.name.search(regexp) < 0)
+							match = false;
+					}, this);	
+
+					if (match)
 						toReturn.push(app);
 				}, this);
+				
 				return toReturn;				
 			},
 			isLoading: function () {
@@ -114,13 +131,17 @@
 	app.controller('appCtrl',[
 		'$scope', 'SteamAppProvider',
 		function($scope, SteamAppProvider){
-			SteamAppProvider.initDB(function(){
-				$scope.isLoading = false;
-			});
 
 			$scope.isLoading = true;
 			$scope.choosenApps = [];
 
+			SteamAppProvider.initDB(function(){
+				$scope.isLoading = false;
+			});
+
+			$scope.clearResults = function(){
+				$scope.apps = [];
+			};
 			$scope.addApp = function(index){
 				var app = $scope.apps[index];
 				$scope.choosenApps.push(app);
@@ -143,16 +164,62 @@
 			};
 
 			$scope.generateText = function(){
-				var redditTable = "|Title|Disc.|$USD|$CAD|€EUR|£GBP|AU ($USD)|BRL$|Metascore|Platform|Cards|PCGW|\n";
-				redditTable += "|:-|-:|-:|-:|-:|-:|-:|-:|-:|:-:|:-:|:-:|";
-				var appInfo = $scope.choosenApps[0].appInfo;
-				redditTable += "|[" + appInfo.name + "](www.google.com)|";
-				redditTable += appInfo.prices['discount_percent'] + "|"+appInfo.prices['us'];
-				redditTable += 'porcaddio ho sonno';
 
-				$scope.redditTable = redditTable;
+				var stringToken = function(string){
+					redditTable += "|"+string;
+				};
+				var urlToken = function(name, url){
+					redditTable += "|[" + name + "](" + url + ")";
+				};
+				var platformToken = function(platforms){
+					redditTable += "|";
+					redditTable += platforms.windows ? 'W' : '';
+					redditTable += platforms.mac ? 'M' : '';
+					redditTable += platforms.linux ? 'L' : '';
+				};
+
+				var redditTable = "|Title|Disc.|$USD|$CAD|€EUR|£GBP|AU ($USD)|BRL$|Metascore|Platform|Cards|PCGW|\n";
+				redditTable += "|:-|-:|-:|-:|-:|-:|-:|-:|-:|:-:|:-:|:-:|\n";
+				
+				for (var i = 0; i < $scope.choosenApps.length; i++) {
+					var appInfo = $scope.choosenApps[i].appInfo;
+					urlToken(appInfo.name, appInfo.appUrl);
+					
+					stringToken(appInfo.prices['us']['discount_percent']);
+
+					stringToken(appInfo.prices['us']['final']/100);
+					stringToken(appInfo.prices['ca']['final']/100);
+					stringToken(appInfo.prices['eur']['final']/100);
+					stringToken(appInfo.prices['uk']['final']/100);
+					stringToken(appInfo.prices['au']['final']/100);
+					stringToken(appInfo.prices['br']['final']/100);
+
+					// stringToken(appInfo.prices['ru']['discount_percent']);
+					// stringToken(appInfo.prices['ru']['final']/100);
+
+					if (appInfo.metacritic){
+						urlToken(appInfo.metacritic.score, appInfo.metacritic.url);
+					}
+					else{
+						stringToken('N/A');
+					}
+
+					platformToken(appInfo.platforms);
+
+					stringToken(appInfo.cards ? 'Yes' : 'No');
+
+					if (appInfo.pcgwUrl){
+						urlToken('YES', appInfo.pcgwUrl);
+					}
+					else{
+						stringToken('No');
+					}
+
+					stringToken('\n');
+
+					$scope.redditTable = redditTable;
+				};
 			};
 		}
 	]);
-
 })();
